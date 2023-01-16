496f1fe3de35b2ec52f1a49f9bbab9ce0f594209
Ansh Phirani <anshphirani@gmail.com>
2023-01-16 20:48:47 +0530

    Time: 1 ms (99.26%), Space: 44.5 MB (86.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -4,17 +4,19 @@ class Solution {
         List<int[]> res = new ArrayList<>();
         
         int idx = 0;
+        boolean isInserted = false;
+        
         while(idx < n){
             int[] curInterval = intervals[idx];
             int commonStart = Math.max(curInterval[0], newInterval[0]);
             int commonEnd = Math.min(curInterval[1], newInterval[1]);
-            if(commonEnd >= commonStart){
+            if(!isInserted && commonEnd >= commonStart){
                 newInterval[0] = Math.min(curInterval[0], newInterval[0]);
                 newInterval[1] = Math.max(curInterval[1], newInterval[1]);
             }
-            else if(newInterval[0] != Integer.MAX_VALUE && newInterval[1] < curInterval[0]){
+            else if(!isInserted && newInterval[1] < curInterval[0]){
                 res.add(newInterval);
-                newInterval = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE}; // added succesfully
+                isInserted = true;
                 res.add(curInterval);
             }
             else{
@@ -23,7 +25,7 @@ class Solution {
             idx++;
         }
         
-        if(newInterval[0] != Integer.MAX_VALUE) res.add(newInterval);
+        if(!isInserted) res.add(newInterval);
 
         int[][] resArray = new int[res.size()][];
         for(int i = 0; i < res.size(); i++){
