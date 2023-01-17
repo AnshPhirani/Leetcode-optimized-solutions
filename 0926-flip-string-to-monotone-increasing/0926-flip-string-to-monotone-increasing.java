51e37e0e97d5e0996950762d3d7604019f9bcfca
Ansh Phirani <anshphirani@gmail.com>
2023-01-17 18:08:59 +0530

    Time: 16 ms (57.29%), Space: 42.5 MB (98.31%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -3,30 +3,21 @@ class Solution {
     public int minFlipsMonoIncr(String s) {
         int n = s.length();
         
-        int[] prefixOnes = new int[n+1];
-        prefixOnes[0] = 0;
-        for(int i = 1; i <= n; i++){
-            prefixOnes[i] = prefixOnes[i-1];
-            if(s.charAt(i-1) == '1'){
-                prefixOnes[i]++;   
-            }
-        }
-        
-        int[] sufixZeros = new int[n+1];
-        sufixZeros[n] = 0;
+        int prefixOnes = 0;
+        int sufixZeros = 0;
         for(int i = n-1; i >= 0; i--){
-            sufixZeros[i] = sufixZeros[i+1];
             if(s.charAt(i) == '0'){
-                sufixZeros[i]++;   
+                sufixZeros++;   
             }
         }
         
         int minFlips = n;
-        for(int i = 0; i <= n; i++){
-            int leftOnes = prefixOnes[i];
-            int rightZeros = sufixZeros[i];
-            minFlips = Math.min(minFlips, leftOnes+rightZeros);
+        for(int i = 0; i < n; i++){
+            minFlips = Math.min(minFlips, prefixOnes+sufixZeros);
+            if(s.charAt(i) == '1') prefixOnes++;
+            else sufixZeros--;
         }
+        minFlips = Math.min(minFlips, prefixOnes); // all zeros case
         
         return minFlips;
     }
