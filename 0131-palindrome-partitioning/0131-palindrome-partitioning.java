6955dde1aa8963aef0a9aa98be91f0184f4684b1
Ansh Phirani <anshphirani@gmail.com>
2023-01-22 17:17:53 +0530

    Time: 8 ms (99.21%), Space: 54.9 MB (64.84%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,13 +1,19 @@
 class Solution {
     List<List<String>> res;
+    boolean[][] isPalindrome;
     
-    private boolean isPalindrome(String str, int s, int e){
-        while(s < e){
-            if(str.charAt(s) != str.charAt(e)) return false;
-            s++;
-            e--;
+    private void buildIsPalindrome(String str, int n){
+        for(int g = 0; g < n; g++){
+            int s = 0;
+            for(int e = g; e < n; s++, e++){
+                if(str.charAt(s) == str.charAt(e)){
+                    if(g == 0 || g == 1 || isPalindrome[s+1][e-1]){
+                        isPalindrome[s][e] = true;
+                    }
+                }
+            }
         }
-        return true;
+        return;
     }
     
     private void helper(int start, String s, List<String> curPartition){
@@ -17,7 +23,7 @@ class Solution {
         }
         
         for(int end = start; end < s.length(); end++){
-            if(isPalindrome(s, start, end)){
+            if(isPalindrome[start][end]){
                 curPartition.add(s.substring(start, end+1));
                 helper(end+1, s, curPartition);
                 curPartition.remove(curPartition.size()-1);
@@ -27,7 +33,12 @@ class Solution {
     }
     
     public List<List<String>> partition(String s) {
-        res = new ArrayList<>();
+        int n = s.length();
+        this.res = new ArrayList<>();
+        this.isPalindrome = new boolean[n][n];
+        
+        buildIsPalindrome(s, n);
+        
         helper(0, s, new ArrayList<>());
         return res;
     }
