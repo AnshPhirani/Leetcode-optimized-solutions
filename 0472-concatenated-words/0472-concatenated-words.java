35fd497407d17e68011dcffc6fb142dc30985c69
Ansh Phirani <anshphirani@gmail.com>
2023-01-27 22:29:29 +0530

    Time: 174 ms (13.95%), Space: 71.3 MB (31.28%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,35 +1,29 @@
 class Solution {
     
     Set<String> words;
-    List<String> res;
-    boolean isCancatenatedWord = false; // pruning to prevent repeated word to push into resulting list
     
-    private void dfs(int start, String word){
-        if(isCancatenatedWord) return;
-        
+    private boolean dfs(int start, String word){
+
         if(start == word.length()){
-            res.add(word);
-            isCancatenatedWord = true;
-            return;
+            return true;
         }
         
         for(int end = start+1; end <= word.length(); end++){
             String curSubstring = word.substring(start, end);  
-            if(words.contains(curSubstring)){
-                dfs(end, word);
+            if(words.contains(curSubstring) && dfs(end, word)){
+                return true;
             }
         }
-        
+        return false;
     }
     
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         this.words = new HashSet<>(Arrays.asList(words));
-        this.res = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         
         for(String word : words){
             this.words.remove(word);
-            this.isCancatenatedWord = false;
-            dfs(0, word);
+            if(dfs(0, word)) res.add(word);
             this.words.add(word);
         }
         
