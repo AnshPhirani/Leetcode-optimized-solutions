a9d697d7aa96ab0165433fa63474ae1efbc38b0b
Ansh Phirani <anshphirani@gmail.com>
2023-01-28 22:17:45 +0530

    Time: 107 ms (44.12%), Space: 46.9 MB (97.90%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
@@ -0,0 +1,41 @@
+class SummaryRanges {
+    
+    TreeSet<Integer> values;
+    
+    public SummaryRanges() {
+        values = new TreeSet<>();
+    }
+    
+    public void addNum(int value) {
+        values.add(value);
+    }
+    
+    public int[][] getIntervals() {
+        if(values.isEmpty()) return new int[0][];
+        
+        List<int[]> intervals = new ArrayList<>();
+        int left = -1, right = -1;
+        for(int value : values){
+            if(left == -1){
+                left = right = value;
+            }
+            else if(value == right+1){
+                right = value;
+            }
+            else{
+                intervals.add(new int[]{left, right});
+                left = right = value;
+            }
+        }
+        
+        intervals.add(new int[]{left, right});
+        return intervals.toArray(new int[0][]);
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(value);
+ * int[][] param_2 = obj.getIntervals();
+ */
