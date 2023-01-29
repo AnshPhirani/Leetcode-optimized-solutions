fc43e4e8ef726697d85ec78d2ed29a04ca50a32f
Ansh Phirani <anshphirani@gmail.com>
2023-01-30 00:18:52 +0530

    Time: 26 ms (39.66%), Space: 39.3 MB (99.43%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -2,9 +2,11 @@ class Solution {
     public boolean circularArrayLoop(int[] nums) {
         int n = nums.length;
         boolean[] visited = new boolean[n];
-        
+        for(int i = 0; i < n; i++) nums[i] %= n;
+ 
         for(int start = 0; start < n; start++){
-            if(Math.abs(nums[start])%n == 0) continue;
+            if(nums[start] == 0) continue; // self loop
+            
             Arrays.fill(visited, false);
             boolean hasPositive = nums[start] > 0;
             int end = (start + nums[start])%n;
