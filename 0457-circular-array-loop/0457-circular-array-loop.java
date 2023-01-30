075860967880721fdb49c764154e83a10671f037
Ansh Phirani <anshphirani@gmail.com>
2023-01-31 02:38:42 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (86.98%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -1,27 +1,44 @@
 class Solution {
+    
+    private int getNext(int[] nums, int i){
+        int n = nums.length;
+        int next = (i+nums[i])%n;
+        if(next < 0) next += n;
+        return next;
+    }
+    
     public boolean circularArrayLoop(int[] nums) {
         int n = nums.length;
-        boolean[] visited = new boolean[n];
-        for(int i = 0; i < n; i++) nums[i] %= n;
- 
-        for(int start = 0; start < n; start++){
-            if(nums[start] == 0) continue; // self loop
+        for(int i = 0; i < n; i++){
+            nums[i] %= n;
+        }
+       
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0) continue; // self loop or already visited
             
-            Arrays.fill(visited, false);
-            boolean hasPositive = nums[start] > 0;
-            int end = (start + nums[start])%n;
-            if(end < 0) end += n; 
-
-            while(end != start){
-                if(hasPositive != (nums[end] > 0)) break;
-                if(visited[end]) break;
-                visited[end] = true;
-                end = (end + nums[end])%n;
-                if(end < 0) end += n; 
+            int slow = getNext(nums, i);
+            int fast = getNext(nums, getNext(nums, i));
+            boolean hasPositives = nums[i] > 0;
+ 
+            
+            while(hasPositives == (nums[fast] > 0) && hasPositives == (nums[slow] > 0) && nums[slow] != 0 && nums[fast] != 0){
+       
+                if(slow == fast) return true;
+                slow = getNext(nums, slow);
+                fast = getNext(nums, fast);
+                if(hasPositives != nums[fast] > 0) break;
+                fast = getNext(nums, fast);
+          
             }
             
-            if(end == start) return true;
+            int cur = i;
+            while(nums[cur] != 0 && hasPositives == nums[cur] > 0){
+                int next = getNext(nums, cur);
+                nums[cur] = 0;
+                cur = next;
+            }
+            
         }
-        return false;
+        return false;   
     }
 }
