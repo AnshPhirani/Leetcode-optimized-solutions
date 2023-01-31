e92c2beb5d92980b2199cf58206bb26a4e9f5cf5
Ansh Phirani <anshphirani@gmail.com>
2023-01-31 23:07:28 +0530

    Time: 218 ms (14.28%), Space: 72.5 MB (10.20%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
@@ -1,4 +1,27 @@
 class Solution {
+    
+    Integer[][] memo;
+    
+    private int helper(int idx, int prev, int[][] players){
+        if(idx == players.length) return 0;
+        
+        if(prev != -1 && memo[idx][prev] != null) return memo[idx][prev];
+        
+        // including the current player
+        int including = 0;
+        if(prev == -1 || players[prev][1] <= players[idx][1]){
+            including = players[idx][1] + helper(idx+1, idx, players);
+        }
+        
+        // exculding the current element
+        int excluding = helper(idx+1, prev, players);
+        
+        
+        if(prev != -1) memo[idx][prev] = Math.max(including, excluding);
+        return Math.max(including, excluding);
+        
+    }
+    
     public int bestTeamScore(int[] scores, int[] ages) {
         int n = ages.length;
         int[][] players = new int[n][2];
@@ -6,22 +29,9 @@ class Solution {
             players[i][0] = ages[i];
             players[i][1] = scores[i];
         }
-        
         Arrays.sort(players, (a, b) -> (a[0] != b[0]) ? a[0] - b[0] : a[1] - b[1]);
         
-        int[] dp = new int[n];
-        
-        int maxScore = 0;
-        for(int i = 0; i < n; i++){
-            dp[i] = players[i][1];
-            for(int j = i-1; j >= 0; j--){
-                if(players[j][1] <= players[i][1]){
-                    dp[i] = Math.max(dp[i], dp[j] + players[i][1]);
-                }
-            }
-            maxScore = Math.max(maxScore, dp[i]);
-        }
-        
-        return maxScore;
+        this.memo = new Integer[n][n];
+        return helper(0, -1, players);
     }
 }
