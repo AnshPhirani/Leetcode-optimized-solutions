e448d7a3d3fc5b05871837e70b06199dbc72a1c8
Ansh Phirani <anshphirani@gmail.com>
2023-02-01 22:47:17 +0530

    Time: 4 ms (20.08%), Space: 42.4 MB (55.91%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -2,30 +2,31 @@ class Solution {
     
     private boolean canDivide(String s, String t){
         int n = t.length();
+        
+        if(s.length()%n != 0) return false;
+        
         int idx = 0;
         for(char ch : s.toCharArray()){
             if(ch != t.charAt(idx)) return false;
             idx = (idx+1)%n;
         }
         
-        return idx == 0;
+        return true;
     }
     
     public String gcdOfStrings(String str1, String str2) {
         int n = str1.length();
         int m = str2.length();
-        if(n < m) return gcdOfStrings(str2, str1);
         
         String gcd = "";
         String prefix = "";
-        for(int i = 0; i < m; i++){
-            if(str2.charAt(i) != str1.charAt(i)) return "";
-            prefix += str2.charAt(i);
-            if(canDivide(str1, prefix) && canDivide(str2, prefix)){
-                gcd = prefix;
+        for(int i = Math.min(n, m); i >= 1; i--){
+            String divisor = str1.substring(0, i);
+            if(canDivide(str1, divisor) && canDivide(str2, divisor)){
+                return divisor;
             }
         }
         
-        return gcd;
+        return "";
     }
 }
