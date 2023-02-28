91de303cfca5d437ed6c8eb90e6a4dd1f337cbfc
Ansh Phirani <anshphirani@gmail.com>
2023-02-28 23:09:59 +0530

    Time: 11 ms (94.80%), Space: 42.9 MB (93.93%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -18,22 +18,27 @@ class Solution {
     List<TreeNode> res;
     
     
-    private String dfs(TreeNode root, Map<String, Integer> freq){
-        if(root == null) return "";
-        String leftSubtree = dfs(root.left, freq);
-        String rightSubtree = dfs(root.right, freq);
+    private int dfs(TreeNode root, Map<Integer, Integer> freq, Map<String, Integer> tripletToId){
+        if(root == null) return 0;
         
-        String curRepresentation = "(" + leftSubtree + ")" + root.val + "(" + rightSubtree + ")"; 
+        int left = dfs(root.left, freq, tripletToId);
+        int right = dfs(root.right, freq, tripletToId);
         
-        freq.put(curRepresentation, freq.getOrDefault(curRepresentation, 0) + 1);
-        if(freq.get(curRepresentation) == 2) res.add(root);
+        String curTriplet = left + "," + root.val + "," + right; 
         
-        return curRepresentation;
+        tripletToId.putIfAbsent(curTriplet, tripletToId.size()+1);
+        
+        int curId = tripletToId.get(curTriplet);
+        
+        freq.put(curId, freq.getOrDefault(curId, 0) + 1);
+        if(freq.get(curId) == 2) res.add(root);
+        
+        return curId;
     }
     
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
         this.res = new ArrayList<>();
-        dfs(root, new HashMap<>());
+        dfs(root, new HashMap<>(), new HashMap<>());
         return res;
     }
 }
