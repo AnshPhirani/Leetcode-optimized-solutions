d0eb7bba8e9995629147558f85a0aca06237b968
Ansh Phirani <anshphirani@gmail.com>
2023-03-27 11:51:07 +0530

    Time: 3 ms (32.07%), Space: 46.1 MB (8.94%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,31 +2,18 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{0, 0, grid[0][0]}); // [i, j, pathSum]
-        int minSum = Integer.MAX_VALUE;
-        
-        int[][] memo = new int[n][m];
-        for(int[] me : memo) Arrays.fill(me, Integer.MAX_VALUE);
-        
-        while(!que.isEmpty()){
-            int[] cur = que.poll();
-            int i = cur[0], j = cur[1], curSum = cur[2];
-            if(i == n-1 && j == m-1){
-                minSum = Math.min(minSum, curSum);
-            }
-            
-            if(i+1 < n && curSum+grid[i+1][j] < memo[i+1][j]){
-                memo[i+1][j] = curSum+grid[i+1][j];
-                que.add(new int[]{i+1, j, curSum+grid[i+1][j]});
-            }
-            if(j+1 < m && curSum+grid[i][j+1] < memo[i][j+1]){
-                memo[i][j+1] = curSum+grid[i][j+1];
-                que.add(new int[]{i, j+1, curSum+grid[i][j+1]});
+        int[][] dp = new int[n][m];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else if(i == 0) dp[i][j] = dp[i][j-1] + grid[i][j];
+                else if(j == 0) dp[i][j] = dp[i-1][j] + grid[i][j];
+                else dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
             }
         }
         
-        return minSum;
+        
+        return dp[n-1][m-1];
     }
 }
