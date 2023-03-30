1e78f4723fc2d5152f20cfc55a3aa70daf6802a9
Ansh Phirani <anshphirani@gmail.com>
2023-03-31 01:42:25 +0530

    Time: 12 ms (52.87%), Space: 42.7 MB (66.85%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.java
+++ 0087-scramble-string/0087-scramble-string.java
@@ -1,35 +1,24 @@
 class Solution {
     
-    Map<String, Boolean> memo = new HashMap<>();
-    
     public boolean isScramble(String s1, String s2) {
         if(s1.equals(s2)) return true;
-        
-        String key = s1 + "*" + s2;
-        if(memo.containsKey(key)) return memo.get(key);
-        
-        int[] freq1 = new int[26];
-        for(char ch : s1.toCharArray()) freq1[ch-'a']++;
-        
-        int[] freq2 = new int[26];
-        for(char ch : s2.toCharArray()) freq2[ch-'a']++;
-        
-        if(!Arrays.equals(freq1, freq2)) return false;
-        
         int n = s1.length();
         
-        for(int i = 1; i < n; i++){
-            if(isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i))){
-                memo.put(key, true);
-                return true;
-            }
-            if(isScramble(s1.substring(0, i), s2.substring(n-i)) && isScramble(s1.substring(i), s2.substring(0, n-i))){
-                memo.put(key, true);
-                return true;    
-            }
+        boolean[][][] dp = new boolean[n][n][n+1];
+        for(int k = 1; k <= n; k++){
+            for(int i = 0; i+k <= n; i++){
+                for(int j = 0; j+k <= n; j++){
+                    if(k == 1) dp[i][j][k] = s1.charAt(i) == s2.charAt(j);
+                    else{
+                        for(int q = 1; q < k && !dp[i][j][k]; q++){
+                            dp[i][j][k] = (dp[i][j][q] && dp[i+q][j+q][k-q]) || (dp[i][j+k-q][q] && dp[i+q][j][k-q]);
+                        }
+                    }
+                }
+            }   
         }
         
-        memo.put(key, false);
-        return false;
+        return dp[0][0][n];
+        
     }
 }
