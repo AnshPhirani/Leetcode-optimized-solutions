c388d8a0e66ab15ecc7b97c18f46ae5c1ad8ffdc
Ansh Phirani <anshphirani@gmail.com>
2023-04-02 22:41:02 +0530

    Time: 88 ms (17.36%), Space: 62.5 MB (36.36%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -1,30 +1,27 @@
 class Solution {
-    
-    private int lowerBound(long spell, int[] potions, long success){
-        int m = potions.length;
-        int l = 0;
-        int h = m;
-        while(l < h){
-            int mid = l + (h-l)/2;
-            if(spell*potions[mid] >= success){
-                h = mid;
-            }
-            else{
-                l = mid+1;
-            }
-        }
-        
-        return l;
-        
-    }
+
     
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         int n = spells.length, m = potions.length;
+        
+        int[][] sortedSpells = new int[n][2];
+        for(int i = 0; i < n; i++){
+            sortedSpells[i][0] = spells[i];
+            sortedSpells[i][1] = i;
+        }
+        
+        Arrays.sort(sortedSpells, (a, b) -> Integer.compare(a[0],b[0]));
         Arrays.sort(potions);
+        
+        
         int[] res = new int[n];
         
-        for(int i = 0; i < n; i++){
-            res[i] = m - lowerBound(spells[i], potions, success);
+        int k = m-1;
+        for(int[] sortedSpell : sortedSpells){
+            int spell = sortedSpell[0];
+            int idx = sortedSpell[1];
+            while(k >= 0 && (long)spell*potions[k] >= success) k--;
+            res[idx] = m-(k+1);
         }
         
         return res;
