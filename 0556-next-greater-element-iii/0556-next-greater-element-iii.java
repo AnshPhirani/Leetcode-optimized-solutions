259de7435eb8a673c6a40cc9a675703a88247f4f
Ansh Phirani <anshphirani@gmail.com>
2023-04-05 17:07:10 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (31.57%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -1,69 +1,34 @@
 class Solution {
-    
-    private void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    
-    private void reverse(int[] arr, int i, int j){
-        while(i < j){
-            swap(arr, i, j);
-            i++;
-            j--;
-        }
-    }
-    
-    public void nextPermutation(int[] arr) {
-        int n = arr.length;
-        
-        int peek = -1;
-        for(int i = n-2; i >= 0; i--){
-            if(arr[i] < arr[i+1]){
-                peek = i;
-                break;
-            }
-        }
-        
-        if(peek == -1){
-            // reverse(arr, 0, n-1);
-            Arrays.fill(arr, 0);
-            arr[n-1] = -1;
-            return;
-        }
-        
-        int idx = peek+1;
-        for(int i = peek+1; i < n; i++){
-            if(arr[i] > arr[peek] && arr[i] <= arr[idx]){
-                idx = i;
-            }
-        }
-        
-        swap(arr, peek, idx);
-        reverse(arr, peek+1, n-1);
-        
-        
-    }
-    
     public int nextGreaterElement(int n) {
-        String num = String.valueOf(n);
-        int len = num.length();
-
-        int[] arr = new int[len];
-        for(int i = 0; i < len; i++){
-            arr[i] = num.charAt(i) - '0';
+        char[] arr = String.valueOf(n).toCharArray();
+        int len = arr.length;
+        
+        int peek = len-2;
+        while(peek >= 0){
+            if(arr[peek] < arr[peek+1]) break;
+            peek--;
         }
         
-        nextPermutation(arr);
-
-        long res = 0;
-        for(int i = 0; i < len; i++){
-            res = res*10 + arr[i];
+        if(peek == -1) return -1; // not possible
+        
+        int i = peek+1;
+        for(int j = peek+1; j < len; j++){
+            if(arr[j] > arr[peek] && arr[j] <= arr[i]){
+                i = j;
+            }
         }
         
-        if(res > Integer.MAX_VALUE) res = -1;
+        // swap
+        char temp = arr[peek];
+        arr[peek] = arr[i];
+        arr[i] = temp;
         
-        return (int) res;
+        Arrays.sort(arr, peek+1, len);
+        
+        long res = Long.parseLong(new String(arr));
+        if(res > Integer.MAX_VALUE) return -1;
+        else return (int) res;
+        
         
     }
 }
