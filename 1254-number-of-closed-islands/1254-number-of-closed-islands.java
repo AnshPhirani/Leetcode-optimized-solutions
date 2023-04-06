fa774b1e9f9ddd4edd17e85836ef7afce3be4326
Ansh Phirani <anshphirani@gmail.com>
2023-04-06 10:42:59 +0530

    Time: 1 ms (100.00%), Space: 43.2 MB (26.80%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,14 +1,14 @@
 class Solution {
     
-    private boolean dfs(int i, int j, int n, int m, int[][] grid, boolean[][] visited){
+    private boolean dfs(int i, int j, int n, int m, int[][] grid){
         if(i >= n || j >= m || i < 0 ||  j < 0) return false;
-        if(grid[i][j] == 1 || visited[i][j] == true) return true;
+        if(grid[i][j] == 1) return true;
         
-        visited[i][j] = true;
+        grid[i][j] = 1;
         
         // use single & here is important 
-        return dfs(i-1, j, n, m, grid, visited) & dfs(i, j+1, n, m, grid, visited)
-            & dfs(i+1, j, n, m, grid, visited) & dfs(i, j-1, n, m, grid, visited);
+        return dfs(i-1, j, n, m, grid) & dfs(i, j+1, n, m, grid)
+            & dfs(i+1, j, n, m, grid) & dfs(i, j-1, n, m, grid);
         
     }
     
@@ -16,11 +16,11 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         
-        boolean[][] visited = new boolean[n][m];
+
         int count = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(!visited[i][j] && grid[i][j] == 0 && dfs(i, j, n, m, grid, visited)){
+                if(grid[i][j] == 0 && dfs(i, j, n, m, grid)){
                     count++;
                 }
             }
