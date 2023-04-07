e79f4630b77601b8757be9be349b4bbc5a59ceec
Ansh Phirani <anshphirani@gmail.com>
2023-04-07 12:33:47 +0530

    Time: 1 ms (100.00%), Space: 43.7 MB (8.66%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,30 +1,35 @@
 class Solution {
     
-    private boolean dfs(int i, int j, int n, int m, int[][] grid){
-        if(i >= n || j >= m || i < 0 ||  j < 0) return false;
-        if(grid[i][j] == 1) return true;
-        
+    private void dfs(int i, int j, int n, int m, int[][] grid){
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 1){
+            return;
+        }
         grid[i][j] = 1;
-        
-        boolean res = true;
-        if(!dfs(i-1, j, n, m, grid)) res = false;
-        if(!dfs(i+1, j, n, m, grid)) res = false;
-        if(!dfs(i, j+1, n, m, grid)) res = false;
-        if(!dfs(i, j-1, n, m, grid)) res = false;
-        
-        return res;
-        
+        dfs(i+1, j, n, m, grid);
+        dfs(i-1, j, n, m, grid);
+        dfs(i, j+1, n, m, grid);
+        dfs(i, j-1, n, m, grid);
     }
     
     public int closedIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         
-
+        for(int j = 0; j < m; j++){
+            if(grid[0][j] == 0) dfs(0, j, n, m, grid);
+            if(grid[n-1][j] == 0) dfs(n-1, j, n, m, grid);
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(grid[i][0] == 0) dfs(i, 0, n, m, grid);
+            if(grid[i][m-1] == 0) dfs(i, m-1, n, m, grid);
+        }
+        
         int count = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 0 && dfs(i, j, n, m, grid)){
+                if(grid[i][j] == 0){
+                    dfs(i, j, n, m, grid);
                     count++;
                 }
             }
