548a936c71cb5593484ab60d513fc1270677a08b
Ansh Phirani <anshphirani@gmail.com>
2023-04-07 12:23:50 +0530

    Time: 8 ms (92.78%), Space: 56 MB (35.41%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,43 +1,36 @@
 class Solution {
     
-    
-    private int dfs(int i, int j, int n, int m, int[][] grid){
-        if(i < 0 || j < 0 || i >= n || j >= m){
-            return -1;
+    private void dfs(int i, int j, int n, int m, int[][] grid){
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0){
+            return;
         }
-        if(grid[i][j] == 0) return 0;
-        
-        if(grid[i][j] == -1) return 0;
-        
-        grid[i][j] = -1;
-        
-        int res = 0;
-        
-        int top = dfs(i-1, j, n, m, grid);
-        if(top == -1) res = -1;
-        
-        int left = dfs(i, j+1, n, m, grid);
-        if(left == -1) res = -1;
-        
-        int bottom = dfs(i+1, j, n, m, grid);
-        if(bottom == -1) res = -1;
-        
-        int right = dfs(i, j-1, n, m, grid);
-        if(right == -1) res = -1;
-        
-        return res == -1 ? -1 : 1 + top+left+bottom+right;
+        grid[i][j] = 0;
+        dfs(i+1, j, n, m, grid);
+        dfs(i-1, j, n, m, grid);
+        dfs(i, j+1, n, m, grid);
+        dfs(i, j-1, n, m, grid);
     }
     
     
     public int numEnclaves(int[][] grid) {
         int n = grid.length, m = grid[0].length;
         
+        for(int j = 0; j < m; j++){
+            if(grid[0][j] == 1) dfs(0, j, n, m, grid);
+            if(grid[n-1][j] == 1) dfs(n-1, j, n, m, grid);
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(grid[i][0] == 1) dfs(i, 0, n, m, grid);
+            if(grid[i][m-1] == 1) dfs(i, m-1, n, m, grid);
+        }
+        
+        
         int res = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
-                    int cur = dfs(i, j, n, m, grid);
-                    if(cur != -1) res += cur;
+                    res++;
                 }
             }
         }
