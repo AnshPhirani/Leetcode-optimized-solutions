ff0eb5acc50fb5889a222629237af530541b27d0
Ansh Phirani <anshphirani@gmail.com>
2023-04-10 00:41:15 +0530

    Time: 71 ms (86.36%), Space: 110 MB (56.36%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
@@ -1,27 +1,10 @@
-
-// {
-    
-// }
-
-
-// 2 -> 3 -> 4
-
-//     9 -> 10 -> 6  -> 4
-    
-// 3 [a-2 c-1] -> 4[a-3 c-1]
-
-// 6 [a-3] -> 4[a-2 c-1]
-
-
 class Solution {
-    
-    private Map<Integer, int[]> memo;
-    private List<List<Integer>> adj;
+
  
     public int largestPathValue(String colors, int[][] edges) {
         int n = colors.length();
-        this.memo = new HashMap<>();
-        this.adj = new ArrayList<>();
+        int[][] count = new int[n][26];
+        List<List<Integer>> adj = new ArrayList<>();
         
         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
         
@@ -36,39 +19,23 @@ class Solution {
             if(inDegree[i] == 0) que.add(i);
         
         int maxColorFreq = 0;
-        Set<Integer> nodesProcessed = new HashSet<>();
+        int nodesProcessed = 0;
         while(!que.isEmpty()){
             int u = que.poll();
-            nodesProcessed.add(u);
-            if(!memo.containsKey(u)){
-                memo.put(u, new int[26]);
-                memo.get(u)[colors.charAt(u) - 'a']++;
-            }
-            int[] curMaxFreq = memo.get(u);
-            
-            for(int i = 0; i < 26; i++)
-                    maxColorFreq = Math.max(maxColorFreq, curMaxFreq[i]);
+            count[u][colors.charAt(u) - 'a']++;
+            maxColorFreq = Math.max(maxColorFreq, count[u][colors.charAt(u)-'a']);
             
             for(int v : adj.get(u)){
-                if(!memo.containsKey(v)){
-                    memo.put(v, new int[26]);
-                    memo.get(v)[colors.charAt(v) -'a']++;
-                }
-                
-                int[] neighMaxFreq = memo.get(v);
-                neighMaxFreq[colors.charAt(v) - 'a']--;
-                for(int i = 0; i < 26; i++){
-                    neighMaxFreq[i] = Math.max(neighMaxFreq[i], curMaxFreq[i]);
-                }
-                neighMaxFreq[colors.charAt(v) - 'a']++;
-                
+                for(int i = 0; i < 26; i++)
+                    count[v][i] = Math.max(count[v][i], count[u][i]);
                 
                 inDegree[v]--;
                 if(inDegree[v] == 0) que.add(v);
             }
+            nodesProcessed++;
         }
         
-        return nodesProcessed.size() == n ? maxColorFreq : -1;
+        return nodesProcessed == n ? maxColorFreq : -1;
         
     }
 }
