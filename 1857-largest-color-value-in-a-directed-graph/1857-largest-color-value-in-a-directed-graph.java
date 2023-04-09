7cc6097eda750c5d7793c32676dd5a8c6507744f
Ansh Phirani <anshphirani@gmail.com>
2023-04-09 22:55:53 +0530

    Time: 150 ms (22.73%), Space: 137.3 MB (18.18%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.java
@@ -2,22 +2,22 @@ class Solution {
     
     Map<Integer, int[]> memo;
     List<List<Integer>> adj;
-    boolean hasCycle;
-    int maxColorFreq;
-    
+
     private int dfs(int u, String colors, boolean[] visited){
         
         if(visited[u]){
-            hasCycle = true;
-            return -1;
+            return -1; // cycle found
         }
         visited[u] = true;
         
         int[] curFreq = new int[26];
         
         for(int v : adj.get(u)){
-            if(!memo.containsKey(v)) dfs(v, colors, visited);
-            if(hasCycle) return -1;
+            if(!memo.containsKey(v)) {
+                int curMaxFreq = dfs(v, colors, visited);
+                if(curMaxFreq == -1) return -1; // has cycle
+            }
+
             int[] nextFreq = memo.get(v);
             for(int i = 0; i < 26; i++){
                 curFreq[i] = Math.max(curFreq[i], nextFreq[i]);
@@ -35,8 +35,6 @@ class Solution {
         int n = colors.length();
         this.memo = new HashMap<>();
         this.adj = new ArrayList<>();
-        this.hasCycle = false;
-        this.maxColorFreq = 0;
         
         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
         
@@ -44,13 +42,14 @@ class Solution {
             adj.get(edge[0]).add(edge[1]);
         }
         
+        int maxColorFreq = 0;
         boolean[] visited = new boolean[n];
         for(int i = 0; i < n; i++){
             if(!memo.containsKey(i)) {
                 int curMaxFreq = dfs(i, colors, visited);
+                if(curMaxFreq == -1) return -1; // cycle
                 maxColorFreq = Math.max(maxColorFreq, curMaxFreq);
             }
-            if(hasCycle) return -1;
         }
 
         return maxColorFreq;
