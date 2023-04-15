e525410b2d693bbb47b46f3d4b9e9fe925b975d9
Ansh Phirani <anshphirani@gmail.com>
2023-04-15 23:47:06 +0530

    Time: 172 ms (70.99%), Space: 45.7 MB (44.44%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -1,32 +1,27 @@
 class Solution {
-    Integer[][] memo;
-    
-    private int dfs(int idx, List<List<Integer>> piles, int k){
-        if(k == 0) return 0;
-        if(idx == piles.size()) return Integer.MIN_VALUE;
-        
-        if(memo[idx][k] != null) return memo[idx][k];
-        
-        int res = Integer.MIN_VALUE;
-        
-        // skip choosing from current pile
-        res = Math.max(res, dfs(idx+1, piles, k));
-        
-        // choose from current pile
-        List<Integer> pile = piles.get(idx);
-        int size = pile.size();
-        int curSum = 0;
-        for(int j = 0; j < Math.min(size, k); j++){
-            curSum += pile.get(j);
-            res = Math.max(res, curSum + dfs(idx+1, piles, k-(j+1)));
-        }
-        
-        return memo[idx][k] = res;
-        
-    }
     
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-        this.memo = new Integer[piles.size()][k+1];
-        return dfs(0, piles, k);
+        int n = piles.size();
+        int[][] dp = new int[n+1][k+1];
+
+        for(int idx = 1; idx <= n; idx++){
+            for(int coins = 0; coins <= k; coins++){
+                // skip choosing from current pile
+                dp[idx][coins] = Math.max(dp[idx][coins], dp[idx-1][coins]);
+                
+                // choosing j coins from current pile
+                List<Integer> curPile = piles.get(idx-1);
+                int size = curPile.size();
+                int curSum = 0;
+                for(int j = 0; j < Math.min(size, coins); j++){
+                    curSum += curPile.get(j);
+                    dp[idx][coins] = Math.max(dp[idx][coins], curSum + dp[idx-1][coins-(j+1)]);
+                }
+                
+            }
+        }
+        
+        return dp[n][k];
+        
     }
 }
