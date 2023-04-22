89f4a5abc1674dcdbe0df01c0df5f7eebb38f74c
Ansh Phirani <anshphirani@gmail.com>
2023-04-22 11:46:06 +0530

    Time: 29 ms (73.12%), Space: 44.5 MB (10.64%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,32 +1,19 @@
 class Solution {
+    Integer[][] memo;
     
-    private Integer[][] memo;
-    
-    
-     private int helper(int s, int e, String str){
-        if(s > e) return 0;
-        if(s == e) return 1;
-        
+    private int dfs(String str, int s, int e){
+        if(s >= e) return 0;
         if(memo[s][e] != null) return memo[s][e];
-        
         if(str.charAt(s) == str.charAt(e)){
-            return memo[s][e] = 2 + helper(s+1, e-1, str);
+            return memo[s][e] = dfs(str, s+1, e-1);
         }
         else{
-            return memo[s][e] = Math.max(helper(s+1, e, str), helper(s, e-1, str));
+            return memo[s][e] = 1+Math.min(dfs(str, s+1, e), dfs(str, s, e-1));
         }
-        
-    }
-    
-    public int longestPalindromeSubseq(String s) {
-        this.memo = new Integer[s.length()][s.length()];
-        
-        return helper(0, s.length()-1, s);
     }
     
     public int minInsertions(String s) {
-
-        return s.length() - longestPalindromeSubseq(s);
+        this.memo = new Integer[s.length()][s.length()];
+        return dfs(s, 0, s.length()-1);
     }
-
 }
