c147de8561dd98c201aae8bfeb2c7880239d47c6
Ansh Phirani <anshphirani@gmail.com>
2023-05-16 22:29:40 +0530

    Time: 1 ms (55.17%), Space: 40.7 MB (5.85%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,23 +1,28 @@
 class Solution {
     private int[] nums;
     
+    Integer[][][] memo;
+    
     private int helper(int l, int r, int player){
         if(l > r) return 0;
+        if(memo[l][r][player] != null) return memo[l][r][player];
         
         if(player == 0){
             int low = nums[l] + helper(l+1, r, 1-player);
             int high = nums[r] + helper(l, r-1, 1-player);
-            return Math.max(low, high);
+            return memo[l][r][player] = Math.max(low, high);
         }
         else{
             int low = -nums[l] + helper(l+1, r, 1-player);
             int high = -nums[r] + helper(l, r-1, 1-player);
-            return Math.min(low, high);
+            return memo[l][r][player] = Math.min(low, high);
         }
     }
     
     public boolean PredictTheWinner(int[] nums) {
+
         this.nums = nums;
+        this.memo = new Integer[nums.length][nums.length][2];
         return helper(0, nums.length-1, 0) >= 0;
     }
 }
