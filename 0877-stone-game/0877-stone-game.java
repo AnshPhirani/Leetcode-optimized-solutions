981627d2532c9e5249ccb6d0d21ab953b7621406
Ansh Phirani <anshphirani@gmail.com>
2023-05-26 22:40:17 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (53.58%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,26 +1,6 @@
 class Solution {
 
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        int[][] dp = new int[n][n];
-        for(int gap = 0; gap < n; gap++){
-            for(int l = 0; l < n-gap; l++){
-                int r = l+gap;
-                if(gap == 0) {
-                    dp[l][r] = piles[l];
-                    continue;
-                }
-                int turn = (n-gap)%2;
-                if(turn == 1){ // alice turn
-                    dp[l][r] = Math.max(piles[l] + dp[l+1][r], piles[r] + dp[l][r-1]);
-                }
-                else{ // bob turn
-                    dp[l][r] = Math.min(-piles[l] + dp[l+1][r], -piles[r] + dp[l][r-1]);
-                }
-            }
-        }
-        
-        return dp[0][n-1] > 0;
-        
+        return true;
     }
 }
