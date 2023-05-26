8ede51ec9d7ba0b66d5bdfccbb48398182170f25
Ansh Phirani <anshphirani@gmail.com>
2023-05-27 00:29:49 +0530

    Time: 80 ms (19.42%), Space: 65.5 MB (5.34%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,31 +1,26 @@
 class Solution {
     
-    Integer[][] memo;
+    Integer[] memo;
     
-    private int dfs(int start, int turn, int[] piles){
+    private int dfs(int start, int[] piles){
         if(start >= piles.length) return 0;
         
-        if(memo[start][turn] != null) return memo[start][turn];
+        if(memo[start] != null) return memo[start];
         
-        int curScore = (turn == 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-        
-        int curSum = 0;
-        for(int idx = start; idx < Math.min(start+3, piles.length); idx++){
-            curSum += piles[idx];
-            if(turn == 0){ // alice turn
-                curScore = Math.max(curScore, curSum + dfs(idx+1, 1, piles));    
-            }
-            else{
-                curScore = Math.min(curScore, -curSum + dfs(idx+1, 0, piles)); 
-            }
+        int res = piles[start]-dfs(start+1, piles);
+        if(start+1 < piles.length){
+            res = Math.max(res, piles[start]+piles[start+1] - dfs(start+2, piles));
+        }
+        if(start+2 < piles.length){
+            res = Math.max(res, piles[start]+piles[start+1]+piles[start+2] - dfs(start+3, piles));
         }
         
-        return memo[start][turn] = curScore;
+        return memo[start] = res;
     }
     
     public String stoneGameIII(int[] piles) {
-        this.memo = new Integer[piles.length][2];
-        int aliceMinusBob = dfs(0, 0, piles);
+        this.memo = new Integer[piles.length];
+        int aliceMinusBob = dfs(0, piles);
         return aliceMinusBob == 0 ? "Tie" : aliceMinusBob > 0 ? "Alice" : "Bob"; 
     }
 }
