23dca4285db43a9a3f1a9d0b5efe299c3198e295
Ansh Phirani <anshphirani@gmail.com>
2023-05-27 23:23:04 +0530

    Time: 317 ms (58.97%), Space: 46.9 MB (14.10%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,19 +1,16 @@
 class Solution {
     Integer[][] memo;
+    int[] prefixSum;
     
     private int helper(int s, int e, int[] stoneValue){
         if(s == e) return 0;
-        
         if(memo[s][e] != null) return memo[s][e];
-        
-        int totalSum = 0;
-        for(int i = s; i <= e; i++) totalSum += stoneValue[i];
-        
+    
         int ans = 0;
-        int leftSum = 0, rightSum = 0;
+    
         for(int mid = s; mid < e; mid++){
-            leftSum += stoneValue[mid];
-            rightSum = totalSum - leftSum;
+            int leftSum = prefixSum[mid+1] - prefixSum[s];
+            int rightSum = prefixSum[e+1] - prefixSum[mid+1];
             if(leftSum < rightSum){
                 ans = Math.max(ans, leftSum+helper(s, mid, stoneValue));
             }
@@ -31,6 +28,13 @@ class Solution {
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
         this.memo = new Integer[n][n];
+        
+        this.prefixSum = new int[n+1];
+        prefixSum[0] = 0;
+        for(int i = 1; i <= n; i++){
+            prefixSum[i] = prefixSum[i-1] + stoneValue[i-1];
+        }
+        
         return helper(0, n-1, stoneValue);
     }
 }
