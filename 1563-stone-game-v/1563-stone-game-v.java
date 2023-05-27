89a4f8b78ea5b2254825000c7d54ca25313914fd
Ansh Phirani <anshphirani@gmail.com>
2023-05-28 00:48:19 +0530

    Time: 333 ms (55.13%), Space: 44.5 MB (23.08%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,40 +1,42 @@
 class Solution {
-    Integer[][] memo;
-    int[] prefixSum;
-    
-    private int helper(int s, int e){
-        if(s == e) return 0;
-        if(memo[s][e] != null) return memo[s][e];
-    
-        int ans = 0;
-    
-        for(int mid = s; mid < e; mid++){
-            int leftSum = prefixSum[mid+1] - prefixSum[s];
-            int rightSum = prefixSum[e+1] - prefixSum[mid+1];
-            if(leftSum < rightSum){
-                ans = Math.max(ans, leftSum+helper(s, mid));
-            }
-            else if(leftSum > rightSum){
-                ans = Math.max(ans, rightSum+helper(mid+1, e));
-            }
-            else{
-                ans = Math.max(ans, leftSum+ Math.max(helper(s, mid), helper(mid+1, e)));
-            }
-        }
-        
-        return memo[s][e] = ans;
-    }
     
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
-        this.memo = new Integer[n][n];
+        int[][] dp = new int[n][n];
         
-        this.prefixSum = new int[n+1];
+        int[] prefixSum = new int[n+1];
         prefixSum[0] = 0;
         for(int i = 1; i <= n; i++){
             prefixSum[i] = prefixSum[i-1] + stoneValue[i-1];
         }
         
-        return helper(0, n-1);
+        for(int g = 0; g < n; g++){
+            for(int s = 0; s < n-g; s++){
+                int e = s+g;
+                if(g == 0){
+                    dp[s][e] = 0;
+                    continue;
+                }
+                
+                int ans = 0;
+                for(int mid = s; mid < e; mid++){
+                    int left = prefixSum[mid+1]-prefixSum[s];
+                    int right = prefixSum[e+1]-prefixSum[mid+1];
+                    if(left > right){
+                        ans = Math.max(ans, right+dp[mid+1][e]);
+                    }
+                    else if(right > left){
+                        ans = Math.max(ans, left+dp[s][mid]);
+                    }
+                    else{
+                        ans = Math.max(ans, left + Math.max(dp[s][mid], dp[mid+1][e]));
+                    }
+                }
+                dp[s][e] = ans;
+            }
+        }
+        
+        return dp[0][n-1];
+        
     }
 }
