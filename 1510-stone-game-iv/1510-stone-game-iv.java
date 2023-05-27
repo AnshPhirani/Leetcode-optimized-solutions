a56d8b464c7f54bb1e6e3597d5b60cea36913c7b
Ansh Phirani <anshphirani@gmail.com>
2023-05-27 16:10:59 +0530

    Time: 61 ms (44.44%), Space: 41.4 MB (59.83%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,20 +1,4 @@
 class Solution {
-    Boolean[] memo;
-    
-    private boolean dfs(int remaining){
-        if(remaining == 0) return false;
-        if(remaining == 1) return true;
-        
-        if(memo[remaining] != null) return memo[remaining];
-        
-        for(int k = 1; k <= Math.sqrt(remaining); k++){
-            if(dfs(remaining-k*k) == false){
-                return memo[remaining] = true; 
-            }
-        }
-        
-        return memo[remaining] = false;
-    }
     
     public boolean winnerSquareGame(int n) {
         Boolean[] dp = new Boolean[n+1];
