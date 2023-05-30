644103d134ced23b04e836eae0842fab70cc3d22
Ansh Phirani <anshphirani@gmail.com>
2023-05-31 01:32:22 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (92.21%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,9 +1,6 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
-        if(columnNumber == 0) return "";
-        if(columnNumber <= 26){
-            return "" + (char)('A' + columnNumber-1);
-        }
+        if(columnNumber <= 0) return "";
         
         String cur = columnNumber%26 == 0 ? "Z" : "" + (char)('A' + columnNumber%26 - 1);
         int remaining = columnNumber%26 == 0 ? columnNumber/26 - 1 : columnNumber/26;
