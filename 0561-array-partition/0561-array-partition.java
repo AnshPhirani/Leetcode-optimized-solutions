8a7ff9c5f399c3ddbeba872e59240d9a40d74d09
Ansh Phirani <anshphirani@gmail.com>
2023-06-05 12:58:11 +0530

    Time: 6 ms (98.22%), Space: 44.3 MB (94.71%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,9 +1,23 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
+        int[] count = new int[10000+10001];
+        for(int num : nums){
+            num += 10000;
+            count[num]++;
+        }
+        
+        int num = 0;
         int sum = 0;
-        for(int i = 0; i < nums.length; i+= 2){
-            sum += nums[i];
+        boolean flag = true;
+        while(num < count.length){
+            if(count[num] > 0){
+                if(flag) sum += num-10000;
+                flag = !flag;
+                count[num]--;
+            }
+            else{
+                num++;
+            }
         }
         return sum;
     }
