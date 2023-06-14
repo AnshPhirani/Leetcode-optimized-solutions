9f3067cecfc6728ae8207e731c8ba84bfa4452a5
Ansh Phirani <anshphirani@gmail.com>
2023-06-14 13:15:40 +0530

    Time: 1104 ms (28.34%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,3 @@
 
-select customer_number from Orders group by customer_number 
-having count(order_number) = (select count(order_number) from Orders group by customer_number order by count(order_number) desc limit 1);
-
-# select customer_number from Orders group by customer_number order by count(order_number) desc limit 1;
+select customer_number from Orders
+group by customer_number order by count(order_number) desc limit 1;
