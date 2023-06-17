9d2382d3c96fea0418d7c9e7ced3dcd0b16d1f32
Ansh Phirani <anshphirani@gmail.com>
2023-06-17 22:00:19 +0530

    Time: 245 ms (60.10%), Space: 77.4 MB (6.62%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -1,27 +1,30 @@
 class Solution {
+    Integer[][] memo;
+    
+    private int helper(int idx, int k, List<List<Integer>> piles){
+        if(idx == piles.size()){
+            return k == 0 ? 0 : Integer.MIN_VALUE;
+        }
+        
+        if(memo[idx][k] != null) return memo[idx][k];
+        
+        int curAns = helper(idx+1, k, piles);
+        
+        List<Integer> pile = piles.get(idx);
+        for(int i = 0; i < Math.min(pile.size(), k); i++){
+            curAns = Math.max(curAns, pile.get(i) + helper(idx+1, k-(i+1), piles));
+        }
+        return memo[idx][k] = curAns;
+    }
     
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-        int n = piles.size();
-        int[][] dp = new int[n+1][k+1];
-
-        for(int idx = 1; idx <= n; idx++){
-            for(int coins = 0; coins <= k; coins++){
-                // skip choosing from current pile
-                dp[idx][coins] = Math.max(dp[idx][coins], dp[idx-1][coins]);
-                
-                // choosing j coins from current pile
-                List<Integer> curPile = piles.get(idx-1);
-                int size = curPile.size();
-                int curSum = 0;
-                for(int j = 0; j < Math.min(size, coins); j++){
-                    curSum += curPile.get(j);
-                    dp[idx][coins] = Math.max(dp[idx][coins], curSum + dp[idx-1][coins-(j+1)]);
-                }
-                
+        for(List<Integer> pile : piles){
+            for(int i = 1; i < pile.size(); i++){
+                pile.set(i, pile.get(i)+pile.get(i-1));
             }
         }
         
-        return dp[n][k];
-        
+        this.memo = new Integer[piles.size()][k+1];
+        return  helper(0, k, piles);
     }
 }
