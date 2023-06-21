a3660ecc4823add89264a80d947e88c64cd8bd04
Ansh Phirani <anshphirani@gmail.com>
2023-06-22 00:42:29 +0530

    Time: 17 ms (76.92%), Space: 54.9 MB (81.54%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -1,35 +1,36 @@
 class Solution {
+    
+    private long getCost(int[] nums, int[] cost, int base){
+        long totalCost = 0;
+        for(int i = 0; i < nums.length; i++){
+            long gap = Math.abs(nums[i] - base);
+            totalCost += gap * cost[i];
+        }
+        return totalCost;
+    }
+    
     public long minCost(int[] nums, int[] cost) {
         int n = nums.length;
-        int[][] numsAndCost = new int[n][2];
-        for(int i = 0; i < n; i++){
-            numsAndCost[i][0] = nums[i];
-            numsAndCost[i][1] = cost[i];
+        
+        int low = Arrays.stream(nums).min().getAsInt();
+        int high = Arrays.stream(nums).max().getAsInt();
+        
+        long ans = getCost(nums, cost, nums[0]);
+        
+        while(low < high){
+            int mid = low + (high-low)/2;
+            long cost1 = getCost(nums, cost, mid);
+            long cost2 = getCost(nums, cost, mid+1);
+            ans = Math.min(cost1, cost2);
+            
+            if(cost1 > cost2){
+                low = mid+1;
+            }
+            else{
+                high = mid;
+            }
         }
-        Arrays.sort(numsAndCost, (a, b) -> a[0]-b[0]);
-        
-        long[] prefixCost = new long[n];
-        prefixCost[0] = numsAndCost[0][1];
-        for(int i = 1; i < n; i++){
-            prefixCost[i] = prefixCost[i-1] + numsAndCost[i][1];
-        }
-        
-        long totalCost = 0;
-        for(int i = 1; i < n; i++){
-            long gap = numsAndCost[i][0]-numsAndCost[0][0];
-            totalCost += gap * numsAndCost[i][1];
-        }
-        
-        long ans = totalCost;
-        
-        for(int i = 1; i < n; i++){
-            long gap = numsAndCost[i][0] - numsAndCost[i-1][0];
-            totalCost += gap * prefixCost[i-1];
-            totalCost -= gap * (prefixCost[n-1] - prefixCost[i-1]);
-            ans = Math.min(ans, totalCost);
-        }
-        
+
         return ans;
-        
     }
 }
