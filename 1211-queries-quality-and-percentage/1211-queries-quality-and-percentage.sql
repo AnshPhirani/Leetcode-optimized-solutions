667a2ce2dd1744a88f10b78abc118650401f1f5a
Ansh Phirani <anshphirani@gmail.com>
2023-06-24 14:10:12 +0530

    Time: 1741 ms (9.47%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,6 +2,6 @@
 SELECT
 query_name,
 ROUND(AVG(rating/position), 2) 'quality',
-ROUND(SUM(IF(rating < 3, 1 , 0))/COUNT(*)*100, 2) 'poor_query_percentage'
+ROUND(SUM(rating < 3)/COUNT(*)*100, 2) 'poor_query_percentage'
 FROM Queries 
 GROUP BY query_name;
