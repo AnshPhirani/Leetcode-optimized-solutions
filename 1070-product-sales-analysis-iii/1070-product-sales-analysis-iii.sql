51689ac1847c791e8e2758f0cd073a36e233a123
Ansh Phirani <anshphirani@gmail.com>
2023-06-28 00:18:55 +0530

    Time: 1790 ms (81.33%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -2,10 +2,10 @@
 WITH FirstYear AS (
     SELECT 
     product_id,
-    MIN(year) 'first_year'
+    MIN(year) 'year'
     FROM Sales GROUP BY product_id
 )
 
 
-SELECT f.product_id, f.first_year, s.quantity, s.price FROM FirstYear f JOIN Sales s 
-ON f.product_id = s.product_id AND f.first_year = s.year;
+SELECT f.product_id, f.year 'first_year', s.quantity, s.price FROM FirstYear f JOIN Sales s 
+ON f.product_id = s.product_id AND f.year = s.year;
