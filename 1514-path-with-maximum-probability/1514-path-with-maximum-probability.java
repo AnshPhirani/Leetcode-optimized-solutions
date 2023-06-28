65079621fa9b607f8d220b4287e3b9d7d781508c
Ansh Phirani <anshphirani@gmail.com>
2023-06-28 23:53:21 +0530

    Time: 2118 ms (5.14%), Space: 59.5 MB (5.30%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,44 +1,42 @@
-class Pair{
-    int node;
-    double prob;
-    public Pair(int node, double prob){
-        this.node = node;
-        this.prob = prob;
+class Solution {
+    
+    double res = 0;
+    List<List<double[]>> adj;
+    boolean[] visited;
+    
+    private void dfs(int start, int end, double probability){
+        if(probability < 1d/100000) return;
+        
+        if(start == end){
+            res = Math.max(res, probability);
+            return;
+        }
+
+        if(visited[start]) return;  
+        
+        visited[start] = true;
+        
+        for(double[] neigh : adj.get(start)){
+            if(!visited[(int)neigh[0]] && neigh[1]*probability >= res)
+                dfs((int)neigh[0], end, neigh[1]*probability);
+        }
+        visited[start] = false;
     }
     
-}
-
-class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-        List<List<Pair>> adj = new ArrayList<>();
+        this.adj = new ArrayList<>();
+        this.visited = new boolean[n];
+        
         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for(int i = 0; i < edges.length; i++){
-            int[] edge = edges[i];
-            adj.get(edge[0]).add(new Pair(edge[1], succProb[i]));
-            adj.get(edge[1]).add(new Pair(edge[0], succProb[i]));
+        
+        int i = 0;
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(new double[]{edge[1], succProb[i]});
+            adj.get(edge[1]).add(new double[]{edge[0], succProb[i]});
+            i++;
         }
         
-        double[] probability = new double[n];
-        Arrays.fill(probability, -1);
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
-            return (b.prob > a.prob) ? 1 : (b.prob < a.prob) ? -1 : 0;
-        });
-        pq.add(new Pair(start, 1.0));
-        
-        while(!pq.isEmpty()){
-            Pair cur = pq.poll();
-            if(probability[cur.node] != -1) continue;
-            probability[cur.node] = cur.prob;
-            
-            for(Pair neigh : adj.get(cur.node)){
-                double newProb = cur.prob * neigh.prob;
-                pq.add(new Pair(neigh.node, newProb));
-            }
-            
-        }
-        
-        return probability[end] != -1 ? probability[end] : 0;
-        
-        
+        dfs(start, end, 1);
+        return res;
     }
 }
