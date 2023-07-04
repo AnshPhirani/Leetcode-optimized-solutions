a321ea2b11d866b7142d4b9f255c42265484ac8c
Ansh Phirani <anshphirani@gmail.com>
2023-07-04 16:15:55 +0530

    Time: 4 ms (66.85%), Space: 44.5 MB (11.40%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,26 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int[] bitsCount = new int[32];
-        int negCount = 0;
-        for(long num : nums){
-            if(num < 0){
-                negCount++;
-                num = -num;
-            }
-            int pos = 0;
-            while(num > 0){
-                if((num&1) != 0) bitsCount[pos]++;
-                num >>= 1;
-                pos++;
+ 
+        for(int i = 0; i < 32; i++){
+            for(int num : nums){
+                if((num & (1 << i)) != 0){
+                    bitsCount[i]++;
+                }
             }
         }
-
+        
+        for(int i = 0; i < 32; i++){
+            bitsCount[i] %= 3;
+        }
         
         int ans = 0;
         for(int i = 0; i < 32; i++){
-            bitsCount[i] %= 3;
-            if(bitsCount[i] == 1) ans += (1<<i);
+            ans += bitsCount[i] * (1 << i);
         }
-        return negCount%3 == 0 ? ans : -ans;
+        
+        return ans;
     }
 }
