8d906ee6aa20adc49504a1f6b7e87dfcf00c6705
Ansh Phirani <anshphirani@gmail.com>
2023-07-27 23:33:32 +0530

    Time: 24 ms (51.82%), Space: 56.3 MB (80.00%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public long maxRunTime(int n, int[] batteries) {
+        Arrays.sort(batteries);
+        long extra = 0;
+        for (int i = 0; i < batteries.length - n; i++) {
+            extra += batteries[i];
+        }
+
+        int[] live = Arrays.copyOfRange(batteries, batteries.length - n, batteries.length);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (extra < (long) (i + 1) * (live[i + 1] - live[i])) {
+                return 1l*live[i] + extra / (i + 1);
+            }
+
+            extra -= (long) (i + 1) * (live[i + 1] - live[i]);
+        }
+
+        return live[n - 1] + extra / n;
+    }
+}
