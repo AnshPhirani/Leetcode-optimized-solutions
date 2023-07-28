0359af4f2ebb85c7f152c3a3e73c867a75534bf3
Ansh Phirani <anshphirani@gmail.com>
2023-07-28 23:52:36 +0530

    Time: 106 ms (10.07%), Space: 39.7 MB (92.26%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,27 +1,16 @@
 class Solution {
-
-    public boolean PredictTheWinner(int[] piles) {
-        int n = piles.length;
-        if(n%2 == 0) return true;
+    
+    private int helper(int s, int e, int[] nums){
+        if(s > e) return 0;
         
-        int[][] dp = new int[n][n];
-        for(int gap = 0; gap < n; gap++){
-            for(int l = 0; l < n-gap; l++){
-                int r = l+gap;
-                if(gap == 0) {
-                    dp[l][r] = piles[l];
-                    continue;
-                }
-                int turn = (n-gap)%2;
-                if(turn == 1){ // player 1 turn
-                    dp[l][r] = Math.max(piles[l] + dp[l+1][r], piles[r] + dp[l][r-1]);
-                }
-                else{ // player 2 turn
-                    dp[l][r] = Math.min(-piles[l] + dp[l+1][r], -piles[r] + dp[l][r-1]);
-                }
-            }
-        }
+        int pickFirst = nums[s] - helper(s+1, e, nums);
+        int pickLast = nums[e] - helper(s, e-1, nums);
+        return Math.max(pickFirst, pickLast);
         
-        return dp[0][n-1] >= 0;
+    }
+    
+    public boolean PredictTheWinner(int[] nums) {
+        int player1Score = helper(0, nums.length-1, nums);
+        return player1Score >= 0;
     }
 }
