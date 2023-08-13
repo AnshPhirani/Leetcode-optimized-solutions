00a399712a59abfdf0323e69fc0f3baefc4b4d94
Ansh Phirani <anshphirani@gmail.com>
2023-08-13 17:01:46 +0530

    Time: 8 ms (75.73%), Space: 56.3 MB (95.15%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,21 +1,14 @@
 class Solution {
     
-    Boolean[] memo;
-    
-    private boolean helper(int idx, int[] nums){
-        if(idx == nums.length) return true;
-        
-        if(memo[idx] != null) return memo[idx];
-        
-        if(idx+1 < nums.length && nums[idx] == nums[idx+1] && helper(idx+2, nums)) return memo[idx] = true;
-        if(idx+2 < nums.length && nums[idx] == nums[idx+1] && nums[idx] == nums[idx+2] && helper(idx+3, nums)) return memo[idx] = true;
-        if(idx+2 < nums.length && nums[idx]+1 == nums[idx+1] && nums[idx+1]+1 == nums[idx+2] && helper(idx+3, nums)) return memo[idx] = true;
-        
-        return memo[idx] = false;
-    }
-    
     public boolean validPartition(int[] nums) {
-        this.memo = new Boolean[nums.length];
-        return helper(0, nums);    
+        boolean[] dp = new boolean[nums.length+1];
+        dp[0] = true;
+        
+        for(int i = 0; i < nums.length; i++){    
+            if(i-1 >= 0 && nums[i] == nums[i-1]) dp[i+1] |= dp[i-1];
+            if(i-2 >= 0 && nums[i] == nums[i-1] && nums[i] == nums[i-2]) dp[i+1] |= dp[i-2];
+            if(i-2 >= 0 && nums[i] == nums[i-1]+1 && nums[i-1] == nums[i-2]+1) dp[i+1] |= dp[i-2];
+        }
+        return dp[nums.length];
     }
 }
