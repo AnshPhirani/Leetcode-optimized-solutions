eae7e1233b414b7fca98aae469fb62fa0ab4c3c9
Ansh Phirani <anshphirani@gmail.com>
2023-08-23 01:20:02 +0530

    Time: 7 ms (15.41%), Space: 40 MB (47.46%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,7 +1,6 @@
 class Solution {
     public String convertToTitle(int n) {
-        System.out.println(n);
-        if(n <= 26) return (char)(n + 'A'-1) + "";
+        if(n <= 0) return "";
         char cur = n%26 == 0 ? 'Z' : (char)(n%26 + 'A'-1);
         int remainingN = n%26 == 0 ? n/26-1 : n/26;
         return convertToTitle(remainingN) + cur;
