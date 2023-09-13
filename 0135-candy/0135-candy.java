659c68ef5b280d842506ccab80c52f58a40cde40
Ansh Phirani <anshphirani@gmail.com>
2023-09-14 01:32:05 +0530

    Time: 2 ms (99.94%), Space: 43.8 MB (53.79%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,22 +2,32 @@ class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
         
-        int[] left = new int[n];
-        left[0] = 1;
-        for(int i = 1; i < n; i++){
-            left[i] = ratings[i] > ratings[i-1] ? left[i-1]+1 : 1;
-        }
+        int idx = 1;
+        int candies = n;
         
-        int[] right = new int[n];
-        right[n-1] = 1;
-        for(int i = n-2; i >= 0; i--){
-            right[i] = ratings[i] > ratings[i+1] ? right[i+1]+1 : 1;
+        while(idx < n){
+            if(ratings[idx] == ratings[idx-1]){
+                 idx++;
+                 continue;
+            }
+            
+            int peak = 0;
+            while(ratings[idx] > ratings[idx-1]){
+                peak++;
+                candies += peak;
+                idx++;
+                if(idx == n) return candies;
+            }
+            
+            int valley = 0;
+            while(idx < n && ratings[idx] < ratings[idx-1]){
+                valley++;
+                candies += valley;
+                idx++;
+            }
+            
+            candies -= Math.min(peak, valley);
         }
-        
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            ans += Math.max(left[i], right[i]);
-        }
-        return ans;
+        return candies;
     }
 }
