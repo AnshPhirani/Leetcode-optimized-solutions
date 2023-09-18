2f388bbb245c248c03310f810b32c8f79c56454b
Ansh Phirani <anshphirani@gmail.com>
2023-09-18 13:34:01 +0530

    Time: 1 ms (99.31%), Space: 44.4 MB (75.35%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,14 +1,29 @@
 class Solution {
+    
+    private int onesCount(int[] arr){
+        int l = 0;
+        int h = arr.length-1;
+        int res = 0;
+        while(l <= h){
+            int mid = l+(h-l)/2;
+            if(arr[mid] == 1){
+                l = mid+1;;
+                res = mid+1;
+            }
+            else{
+                h = mid-1;
+            }
+        }
+        return res;
+    }
+    
+    
     public int[] kWeakestRows(int[][] mat, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] == b[1] ? b[0]-a[0] : b[1]-a[1]);
         int[] ans = new int[k];
         
         for(int i = 0; i < mat.length; i++){
-            int soldiersCount = 0;
-            for(int val : mat[i]){
-                if(val == 0) break;
-                soldiersCount++;
-            }
+            int soldiersCount = onesCount(mat[i]);
             pq.add(new int[]{i, soldiersCount});
             if(pq.size() > k) pq.poll();    
         }   
