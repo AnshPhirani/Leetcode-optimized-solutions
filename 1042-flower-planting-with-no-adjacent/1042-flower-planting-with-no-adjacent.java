988aacadd3cf671247368c423f504f53cbe70412
Ansh Phirani <anshphirani@gmail.com>
2023-10-10 01:31:59 +0530

    Time: 23 ms (38.62%), Space: 54.7 MB (35.73%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.java
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.java
@@ -1,25 +1,5 @@
 class Solution {
     
-    private void dfs(int src, int[] res, Set<Integer>[] adj){
-        if(res[src] != -1) return;
-        
-        Set<Integer> neighFlowers = new HashSet<>();
-        for(int neigh : adj[src]){
-            neighFlowers.add(res[neigh]);
-        }
-        for(int flower = 1; flower <= 4; flower++){
-            if(!neighFlowers.contains(flower)){
-                res[src] = flower;
-                break;
-            }
-        }
-        
-        for(int neigh : adj[src]){
-            if(res[neigh] == -1) dfs(neigh, res, adj);
-        }
-        
-    }
-    
     public int[] gardenNoAdj(int n, int[][] paths) {
         Set<Integer>[] adj = new Set[n];
         for(int i = 0; i < n; i++) adj[i] = new HashSet<>();
@@ -30,9 +10,18 @@ class Solution {
         }
         
         int[] res = new int[n];
-        Arrays.fill(res, -1);
+
         for(int i = 0; i < n; i++){
-            if(res[i] == -1) dfs(i, res, adj);
+            Set<Integer> neighFlowers = new HashSet<>();
+            for(int neigh : adj[i]){
+                neighFlowers.add(res[neigh]);
+            }
+            for(int flower = 1; flower <= 4; flower++){
+                if(!neighFlowers.contains(flower)){
+                    res[i] = flower;
+                    break;
+                }
+            }
         }
         
         return res;
