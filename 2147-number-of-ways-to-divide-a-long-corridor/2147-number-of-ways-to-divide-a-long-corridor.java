80b0d06a4bb3079a7b1bda24f803dc30bd1d9f0e
Ansh Phirani <anshphirani@gmail.com>
2023-11-29 01:03:30 +0530

    Time: 21 ms (97.30%), Space: 44.2 MB (94.59%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -1,22 +1,29 @@
 class Solution {
     public int numberOfWays(String corridor) {
         int totalSeats = 0;
-        int prevSeat = 0;
+        int curSeats = 0;
+        int curPlants = 0;
         long res = 1;
         
-        for(int i = 0; i < corridor.length(); i++){
-            char ch = corridor.charAt(i);
+        for(char ch : corridor.toCharArray()){
             if(ch == 'S'){
                 totalSeats++;
-                if(totalSeats > 2 && totalSeats%2 == 1){
-                    res *= (i-prevSeat);
+                if(curSeats == 2){
+                    res *= (curPlants+1);
                     res %= (int)1e9+7;
+                    curPlants = 0;
+                    curSeats = 1;
                 }
-                prevSeat = i;
+                else{
+                    curSeats++;
+                }
+            }
+            else{
+                if(curSeats == 2) curPlants++;
             }
         }
         
-        return totalSeats > 1 && totalSeats%2 == 0 ? (int)res : 0;
+        return totalSeats%2 != 0 || totalSeats == 0 ? 0 : (int)res;
         
     }
 }
