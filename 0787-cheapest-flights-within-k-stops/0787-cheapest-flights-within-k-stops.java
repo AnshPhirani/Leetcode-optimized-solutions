da63f1a6dd280150bc8d37be9fdf6cc67f4b4a1f
Ansh Phirani <anshphirani@gmail.com>
2024-02-23 22:14:53 +0530

    Time: 5 ms (96.27%), Space: 44.4 MB (52.73%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,40 +1,40 @@
 class Solution {
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        
-        for(int[] flight : flights){
-            int from = flight[0], to = flight[1], cost = flight[2];
-            adj.get(from).add(new int[]{to, cost});
+        ArrayList<int[]>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
         }
-        
+        for (int[] flight : flights) {
+            adj[flight[0]].add(new int[] { flight[1], flight[2] });
+        }
+
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[] { src, 0, k });
         int[] minCost = new int[n];
         Arrays.fill(minCost, Integer.MAX_VALUE);
-        
-        Queue<int[]> que =  new LinkedList<>();
-        que.add(new int[]{src, 0});
+        minCost[src] = 0;
+
         int level = 0;
-        
-        while(!que.isEmpty() && level <= k){
+        while (!que.isEmpty() && level <= k) {
             int size = que.size();
-            while(size-- > 0){
-                int curNode = que.peek()[0];
-                int curCost = que.poll()[1];
-                
-                for(int[] neighbour : adj.get(curNode)){
-                    int neighNode = neighbour[0];
-                    int cost = neighbour[1];
-                    if(curCost + cost < minCost[neighNode]){
-                        minCost[neighNode] = curCost+cost;
-                        que.add(new int[]{neighNode, minCost[neighNode]});
+            while (size-- > 0) {
+                int[] cur = que.poll();
+                int curCity = cur[0], curPrice = cur[1], remainingK = cur[2];
+
+                // if(remainingK < 0) continue;
+                for (int[] neigh : adj[curCity]) {
+                    int nextCity = neigh[0], cost = neigh[1];
+                    if (curPrice + cost < minCost[nextCity]) {
+                        que.add(new int[] { nextCity, curPrice + cost, remainingK - 1 });
+                        minCost[nextCity] = curPrice + cost;
                     }
                 }
-                
             }
+
             level++;
         }
-        
-        return minCost[dst] != Integer.MAX_VALUE ? minCost[dst] : -1;
-        
+
+        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
     }
 }
