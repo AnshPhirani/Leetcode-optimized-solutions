831b7649e5db13fc8b2a5d2e9637add2717c854e
Ansh Phirani <anshphirani@gmail.com>
2024-08-15 23:20:43 +0530

    Time: 1 ms (100.00%), Space: 55.8 MB (48.09%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int count5 = 0, count10 = 0, count20 = 0;
+        for(int bill : bills){
+            if(bill == 5) count5++;
+            else if(bill == 10){
+                if(count5 == 0) return false;
+                count10++;
+                count5--;
+            }
+            else{
+                 if(count10 > 0 && count5 > 0){
+                     count10--;
+                     count5--;
+                 }
+                else if(count5 >= 3){
+                    count5 -= 3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+} 
