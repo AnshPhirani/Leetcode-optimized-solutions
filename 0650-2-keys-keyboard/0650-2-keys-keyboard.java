aa71c2cadd49bf7869b16fe505b2aa62d5b0d80c
Ansh Phirani <anshphirani@gmail.com>
2024-08-19 22:58:12 +0530

    Time: 6 ms (57.86%), Space: 54.2 MB (15.59%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,22 +1,26 @@
 class Solution {
     
+    Integer[][] memo;
     
     private int helper(int str, int copy, int n){
         if(str == n) return 0;
         if(str > n) return 99999999;
         
+        if(memo[str][copy] != null) return memo[str][copy];
+        
         // paste and don't copy current screen 
         int withOutCopy = helper(str+copy, copy, n);
         
         // paste and copy the current scrren
-        int withCopy = helper(str+copy, str+copy, n);
+        int withCopy = helper(str*2, str, n);
         
-        return Math.min(1 + withOutCopy, 2 + withCopy);
+        return memo[str][copy] = Math.min(1 + withOutCopy, 2 + withCopy);
     }
     
     
     public int minSteps(int n) {
         if(n == 1) return 0;
+        this.memo = new Integer[n][n/2 + 1];
         return 1 +helper(1, 1, n);
         
     }
