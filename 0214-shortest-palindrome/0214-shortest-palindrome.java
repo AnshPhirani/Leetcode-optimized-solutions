e090ab0b3401a8f6cf263b7540f719484dab0d9d
Ansh Phirani <anshphirani@gmail.com>
2024-09-21 01:34:09 +0530

    Time: 176 ms (29.88%), Space: 44.5 MB (86.67%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        int i = 0, end = s.length() - 1, j = end; char chs[] = s.toCharArray();
+        while(i < j) {
+             if (chs[i] == chs[j]) {
+                 i++; j--;
+             } else { 
+                 i = 0; end--; j = end;
+             }
+        }
+        return new StringBuilder(s.substring(end+1)).reverse().toString() + s;
+    }
+}
