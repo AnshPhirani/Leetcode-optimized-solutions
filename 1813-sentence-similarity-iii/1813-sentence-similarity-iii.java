5ce188ff1395019623f5d2719b39ee4417bb0a12
Ansh Phirani <anshphirani@gmail.com>
2024-10-06 21:28:39 +0530

    Time: 1 ms (96.43%), Space: 41.7 MB (39.29%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        if(sentence1.length() > sentence2.length())
+            return areSentencesSimilar(sentence2, sentence1);
+        
+        String[] smaller = sentence1.split(" ");
+        String[] larger = sentence2.split(" ");
+        
+        int start = 0;
+        while(start < smaller.length){
+            if(!smaller[start].equals(larger[start])) break;
+            start++;
+        }
+        
+        int end = smaller.length-1;
+        int end2 = larger.length-1;
+        while(end >= start){
+            if(!smaller[end].equals(larger[end2])) break;
+            end--;
+            end2--;
+        }
+        
+        if(end >= start) return false;
+        
+        return true;
+        
+    }
+}
