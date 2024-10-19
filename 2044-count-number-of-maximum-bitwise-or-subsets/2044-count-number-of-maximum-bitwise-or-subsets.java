ee69431b1aabf888cdf927e67a806d664df3ea2c
Ansh Phirani <anshphirani@gmail.com>
2024-10-19 23:30:56 +0530

    Time: 8 ms (86.08%), Space: 41.4 MB (42.33%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -0,0 +1,17 @@
+class Solution {
+    int maxOR;
+    
+    private int helper(int i, int OR, int[] nums){
+        if(i == nums.length){
+            return OR == maxOR ? 1 : 0;
+        }
+        
+        return helper(i+1, OR | nums[i], nums) + helper(i+1, OR, nums);
+    }
+    
+    public int countMaxOrSubsets(int[] nums) {
+        this.maxOR = 0;
+        for(int num : nums) this.maxOR |= num;
+        return helper(0, 0, nums);
+    }
+}
