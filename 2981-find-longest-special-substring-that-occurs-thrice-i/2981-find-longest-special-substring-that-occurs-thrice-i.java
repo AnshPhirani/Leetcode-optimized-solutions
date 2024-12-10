7d8a9f767262b9a3f70db9934fe3502ea00d39fb
Ansh Phirani <anshphirani@gmail.com>
2024-12-10 23:13:21 +0530

    Time: 90 ms (5.63%), Space: 45.5 MB (11.90%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,21 +1,24 @@
 class Solution {
- 
+    
+    private boolean hasFreqThrice(String substring, String s){
+        int count = 0;
+        int len = substring.length();
+        for(int i = 0; i <= s.length()-len; i++){
+            if(s.substring(i, i+len).equals(substring)) count++;
+        }
+        return count >= 3;
+    }
+    
     public int maximumLength(String s) {
         int n = s.length();
-        
-        Map<String, Integer> freq = new HashMap<>();
+        int ans = -1;
         for(int start = 0; start < n; start++){
             String curSubstring = "";
             for(int end = start; end < n; end++){
                 if(end > start && s.charAt(end) != s.charAt(end-1)) break;
                 curSubstring += s.charAt(end);
-                freq.put(curSubstring, freq.getOrDefault(curSubstring, 0) + 1);
+                if(hasFreqThrice(curSubstring, s)) ans = Math.max(ans, end-start+1);
             }
-        }
-        
-        int ans = -1;
-        for(String key : freq.keySet()){
-            if(freq.get(key) >= 3) ans = Math.max(ans, key.length());
         }
         
         return ans;
