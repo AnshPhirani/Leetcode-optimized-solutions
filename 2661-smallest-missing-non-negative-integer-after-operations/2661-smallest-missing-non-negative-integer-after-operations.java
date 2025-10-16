c987865add5e36ef9dbde1fa8f92b1e9254b87b2
Ansh Phirani <anshphirani@gmail.com>
2025-10-17 00:45:22 +0530

    Time: 93 ms (12.18%), Space: 60.7 MB (43.66%) - LeetHub

---
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findSmallestInteger(int[] nums, int value) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            num = num%value;
+            if(num < 0) num += value;
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+
+        for(int mex = 0; mex <= nums.length; mex++){
+            if(map.containsKey(mex)){
+                map.put(mex, map.get(mex)-1);
+                if(map.get(mex) == 0) map.remove(mex);
+            }
+            else{
+                if(map.containsKey(mex%value)){
+                    map.put(mex%value, map.get(mex%value)-1);
+                    if(map.get(mex%value) == 0) map.remove(mex%value);
+                }
+                else{
+                    return mex;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
