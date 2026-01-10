b52a2d7d658550f06b6d35789a88bac3ab277a1b
Ansh Phirani <anshphirani@gmail.com>
2026-01-10 15:26:13 +0530

    Time: 15 ms (91.65%), Space: 46.7 MB (27.72%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,26 +1,24 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         int n = s1.length(), m = s2.length();
-        
         int[][] dp = new int[n+1][m+1];
-        for(int i = n; i >= 0; i--){
-            for(int j = m; j >= 0; j--){
-                if(i == n || j == m){
-                    if(i == n && j == m) dp[i][j] = 0;
-                    else if(i == n) dp[i][j] = s2.charAt(j) + dp[i][j+1];
-                    else dp[i][j] = s1.charAt(i) + dp[i+1][j];
-                }
-                else if(s1.charAt(i) == s2.charAt(j)){
-                    dp[i][j] = dp[i+1][j+1];
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                int ch1 = (int)s1.charAt(i-1);
+                int ch2 = (int)s2.charAt(j-1);
+                if(ch1 == ch2){
+                    dp[i][j] = dp[i-1][j-1] + ch1;
                 }
                 else{
-                    int deleteFirst = s1.charAt(i) + dp[i+1][j];
-                    int deleteSecond = s2.charAt(j) + dp[i][j+1];
-                    dp[i][j] = Math.min(deleteFirst, deleteSecond);
+                    dp[i][j] =  Math.max(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
-        
-        return dp[0][0];
+
+        int totalAscii = 0;
+        for(char ch : s1.toCharArray()) totalAscii += (int)ch;
+        for(char ch : s2.toCharArray()) totalAscii += (int)ch;
+        return totalAscii - 2*dp[n][m];
     }
 }
