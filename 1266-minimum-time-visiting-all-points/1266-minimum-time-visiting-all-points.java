7788b9ff5b0ecfa3e7df4fd6a897015199d9543e
Ansh Phirani <anshphirani@gmail.com>
2026-01-13 00:26:59 +0530

    Time: 1 ms (95.28%), Space: 45 MB (15.5%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -4,9 +4,7 @@ class Solution {
         for(int idx = 1; idx < points.length; idx++){
             int x1 = points[idx-1][0], y1 = points[idx-1][1];
             int x2 = points[idx][0], y2 = points[idx][1];
-            int dialgonalMoves = Math.min(Math.abs(x2-x1), Math.abs(y2-y1));
-            int straightMoves = Math.max(Math.abs(x2-x1), Math.abs(y2-y1)) - dialgonalMoves;
-            seconds += dialgonalMoves + straightMoves;
+            seconds += Math.max(Math.abs(x2-x1), Math.abs(y2-y1));
         }
         return seconds;
     }
